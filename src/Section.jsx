88abf04689357c72935f27669cbc60907882b9ec
Lucy Farnik <lucyfarnik@gmail.com>
2022-03-15 12:46:10 +0000

    Increased lazyload offset

--- src/Section.jsx
+++ src/Section.jsx
@@ -37,7 +37,7 @@ export default function Section({
             {background && (
                 <div className="absolute top-0 right-0 bottom-0 left-0 overflow-hidden">
                     {lazyload ? (
-                        <LazyLoad once offset={1000}>
+                        <LazyLoad once offset={1500}>
                             <div dangerouslySetInnerHTML={createVideoHTML()} />
                         </LazyLoad>
                     ) : (
