6bdb44e3cef5ea1bf1006b4eefdac7d6735e4f21
Lucy Farnik <lucyfarnik@gmail.com>
2022-03-16 17:19:11 +0000

    Added Yaroslav's contact info

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -5,7 +5,7 @@ export default function Contact() {
         <div>
             <h1>Contact</h1>
             <div className="md:flex md:justify-evenly mb-10">
-                <div className="w-80 mb-10 mx-auto">
+                <div className="md:w-60 mb-10 px-2 mx-auto">
                     <h3>Stand With Ukraine Bristol</h3>
                     <div className="flex justify-around">
                         <Link to="https://fb.me/e/1kNwbl9k1">Facebook</Link>
@@ -15,7 +15,20 @@ export default function Contact() {
                         <Link to="mailto:info@standwithukrainebristol.co.uk">Email</Link>
                     </div>
                 </div>
-                <div className="w-80 mb-10 mx-auto">
+                <div className="md:w-60 mb-10 px-2 mx-auto">
+                    <h3>Yaroslav Korniyets</h3>
+                    <p>Organizer</p>
+                    <p className="text-sm">
+                        If you have questions about the timeline, demands
+                        or have a media query contact Yaroslav.
+                    </p>
+                    <div className="flex justify-center">
+                        <Link to="mailto:aeroslav@yahoo.com">
+                            Email
+                        </Link>
+                    </div>
+                </div>
+                <div className="md:w-60 mb-10 px-2 mx-auto">
                     <h3>Lucy Farnik</h3>
                     <p>Organizer, web designer</p>
                     <p className="text-sm">
